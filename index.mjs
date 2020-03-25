99577c0f4197f54bfaf144e011b2eb0f092434ff
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-03-25 15:04:03 +0100

    binary file lib only

--- index.mjs
+++ index.mjs
@@ -1,15 +1,7 @@
 import { BinaryFile } from "./files/BinaryFile.mjs"
-import GLBFile from "./files/GLBFile.mjs"
-import GLTFFile from "./files/GLTFFile.mjs"
-import MTLFile from "./files/MTLFile.mjs"
-import OBJFile from "./files/OBJFile.mjs"
 import { TextFile } from "./files/TextFile.mjs"
 
 export {
     BinaryFile,
-    GLBFile,
-    GLTFFile,
-    MTLFile,
-    OBJFile,
     TextFile,    
 }
