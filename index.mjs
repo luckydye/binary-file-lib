1d38b56c6e567eff14a52afeffb88fe63abeb4f7
Tim Havlicek <tim.h4vlicek@gmail.com>
2020-03-25 14:59:30 +0100

    move S3Texture to vsource-decomp

--- index.mjs
+++ index.mjs
@@ -1,19 +1,15 @@
 import { BinaryFile } from "./files/BinaryFile.mjs"
-import FBXFile from "./files/FBXFile.mjs"
 import GLBFile from "./files/GLBFile.mjs"
 import GLTFFile from "./files/GLTFFile.mjs"
 import MTLFile from "./files/MTLFile.mjs"
 import OBJFile from "./files/OBJFile.mjs"
-import { S3Texture } from "./files/S3Texture.mjs"
 import { TextFile } from "./files/TextFile.mjs"
 
 export {
     BinaryFile,
-    FBXFile,
     GLBFile,
     GLTFFile,
     MTLFile,
     OBJFile,
-    S3Texture,
     TextFile,    
 }
